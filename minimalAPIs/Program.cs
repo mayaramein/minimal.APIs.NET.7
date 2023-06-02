cddd00ee926f058c3b762c61d155164115063dbf
Mayar Amein <mayaramein@outlook.com>
2023-06-02 12:50:58 +0300

    automapper

--- minimalAPIs/Program.cs
+++ minimalAPIs/Program.cs
@@ -1,6 +1,11 @@
+using minimalAPIs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(typeof(MappingConfig));
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -8,23 +13,6 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
 
 app.Run();
 
